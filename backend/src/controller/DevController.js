5c38a75dab698a159be4d11b198327cf70e9fc0b
William Roger <williamroger.frontend@gmail.com>
2020-04-25 23:07:48 -0300

    Adicionado Métodos Like, Dislike e Listagem de Devs

--- backend/src/controller/DevController.js
+++ backend/src/controller/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } },
+      ],
+    });
+
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
 
@@ -11,18 +27,20 @@ module.exports = {
       return res.json(userExists);
     }
 
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
     const { name, bio, avatar_url: avatar } = response.data;
 
     const dev = await Dev.create({
       name,
       user: username,
       bio,
-      avatar
+      avatar,
     });
 
     return res.json(dev);
-  }
-}
+  },
+};
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,13 @@ const express = require('express');
 const routes = express.Router();
 
 const DevController = require('./controller/DevController');
+const LikeController = require('./controller/LikeController');
+const DislikeController = require('./controller/DislikeController');
 
-routes.post("/devs", DevController.store);
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
 
 module.exports = routes;
