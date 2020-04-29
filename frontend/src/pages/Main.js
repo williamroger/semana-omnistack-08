dee5dd994493f7770382c7b4c5e4e2066c261703
William Roger <williamroger.frontend@gmail.com>
2020-04-29 02:55:30 -0300

    Adicionada rota Devs (React.js)

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,94 @@
 import React from 'react';
+import './Main.css';
 
-export default function Main() {
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+export default function Main({ match }) {
   return (
-    <h1>Hello World</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>
+              Vou deixar você apaixonado por programação! Vou deixar você
+              apaixonado por programação! Vou deixar você apaixonado por
+              programação!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Vou deixar você apaixonado por programação!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Vou deixar você apaixonado por programação!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Vou deixar você apaixonado por programação!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
